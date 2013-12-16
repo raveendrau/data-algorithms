aba4334e5d45927f895fa2014066ec5e3a95a11a
Keng <me@keng.me>
2013-12-16 13:29:07 -0500

    Added Oct23 and Oct28 classes

---
+++ Oct23/src/Divide.java
@@ -0,0 +1,13 @@
+
+public class Divide {
+
+	public Divide() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {	
+		int foo = 5/2;
+		System.out.println(foo);
+	}
+
+}
