2cd02fe3a72760ce65804583dcc90572e8ed60dd
Keng <me@keng.me>
2013-12-21 15:03:12 -0500

    Initialized PrefixCode

--- APTs/src/PrefixCode.java
+++ APTs/src/PrefixCode.java
@@ -1,7 +1,16 @@
 
 public class PrefixCode {
 
-	public PrefixCode() {
+	/**
+	 * Given a String[] words, return the String "Yes" if 
+	 * that set of words is a prefix code or return the String "No, i" 
+	 * if it is not, where i is replaced by the lowest 0-based index of a 
+	 * String in words that is a prefix of another String in words. 
+	 * (That index should have no extra leading zeros.)
+	 * @param words
+	 * @return
+	 */
+	public String isOne(String[] words) {
 		// TODO Auto-generated constructor stub
 	}
 
