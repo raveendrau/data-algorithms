464dd91fe823ce7c61ae50464ded22f7f87ba50e
Keng <me@keng.me>
2013-12-22 14:45:18 -0500

    Initialized GoodWordOnBoardFinder

--- Boggle/src/GoodWordOnBoardFinder.java
+++ Boggle/src/GoodWordOnBoardFinder.java
@@ -1,8 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 
-public class GoodWordOnBoardFinder {
 
-	public GoodWordOnBoardFinder() {
-		// TODO Auto-generated constructor stub
+public class GoodWordOnBoardFinder implements IWordOnBoardFinder{
+
+	
+	public List<BoardCell> cellsForWord(BoggleBoard board, String word) {
+		List<BoardCell> list = new ArrayList<BoardCell>();
+		for (int r = 0; r < board.size(); r++) {
+			for (int c = 0; c < board.size(); c++) {
+				if (helper(board, r, c, list, ...)) {
+					
+				}
+			}
+		}
 	}
 
 }
