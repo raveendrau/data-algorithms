110bd0c7ebee4e2b98df6f348c5823c9e82436d4
Keng <me@keng.me>
2013-12-22 19:56:27 -0500

    Implemented wordStatus for BinarySearchLexicon

--- Boggle/src/BinarySearchLexicon.java
+++ Boggle/src/BinarySearchLexicon.java
@@ -28,9 +28,52 @@ public class BinarySearchLexicon implements ILexicon {
         return wordStatus(s.toString());
     }
 
+    
+    /**
+     * The existing code stores words in a ArrayList. 
+     * You’ll need to modify the code to sort in method.wordStatus 
+     * to use Collections.binarySearch to search the list.
+     * 
+     * Note that when the index value returned is less than zero, 
+     * the value can be used to determine 
+     * where a word should be in a sorted list. 
+     * For example, when looking up “ela”, 
+     * the value returned might be -137. 
+     * This means that “ela” is not in the lexicon, 
+     * but if it were to be inserted it would be at index 136. 
+     * This also means that if the word at index 136 
+     * does not begin with “ela”,
+     * then no word in the lexicon has a prefix of “ela”.  
+     */
     public LexStatus wordStatus(String s) {
         
         // You need to make this code use Binary Search
+        int index = Collections.binarySearch(myWords, s);
+        /**
+         * So, any non-negative value returned by binarySearch 
+         * means the status of a word is LexStatus.WORD.
+         */
+        if (index >= 0) {
+        	return LexStatus.WORD;
+		}
+    	
+        /**
+         * If the value returned is negative, 
+         * one call of the appropriate String.startsWith() 
+         * method can determine if LexStatus.PREFIX 
+         * should be returned (make sure you don’t go off 
+         * the end of the array of words in 
+         * the lexicon when calling startsWith).
+         */
+        else {
+        	int insertionPoint = (index * -1) - 1;
+        	if (insertionPoint < myWords.size()) {
+				if (myWords.get(insertionPoint).startsWith(s)) {
+					return LexStatus.PREFIX;
+				}
+			}
+        }
+        
         
         return LexStatus.NOT_WORD;
     }
