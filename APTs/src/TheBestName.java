86f34a83f05bc918cbda8d6e24d2aab75317c7eb
Keng <me@keng.me>
2013-12-26 13:14:04 -0500

    Wrote two helper functions

--- APTs/src/TheBestName.java
+++ APTs/src/TheBestName.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class TheBestName {
 
@@ -11,7 +15,58 @@ public class TheBestName {
 	 * the name JOHN is the best name of all.
 	 */
 	
-	public 
+	public static int weigh(String name) {
+		Map<Character, Integer> alphabet = new HashMap<Character, Integer>();
+		alphabet.put('A', 1);
+		alphabet.put('B', 2);
+		alphabet.put('C', 3);
+		alphabet.put('D', 4);
+		alphabet.put('E', 5);
+		alphabet.put('F', 6);
+		alphabet.put('G', 7);
+		alphabet.put('H', 8);
+		alphabet.put('I', 9);
+		alphabet.put('J', 10);
+		alphabet.put('K', 11);
+		alphabet.put('L', 12);
+		alphabet.put('M', 13);
+		alphabet.put('N', 14);
+		alphabet.put('O', 15);
+		alphabet.put('P', 16);
+		alphabet.put('Q', 17);
+		alphabet.put('R', 18);
+		alphabet.put('S', 19);
+		alphabet.put('T', 20);
+		alphabet.put('U', 21);
+		alphabet.put('V', 22);
+		alphabet.put('W', 23);
+		alphabet.put('X', 24);
+		alphabet.put('Y', 25);
+		alphabet.put('Z', 26);
+		
+		int weight = 0;
+		for(char c: name.toCharArray())
+			weight = weight + alphabet.get(c);
+		return weight;
+	}
+	
+	/**
+	 * 
+	 * @param name1, which is the name already in the list
+	 * @param name2, which is the name to be added to the list
+	 * @return 0, if name1 is heavier
+	 * @return 1, if name2 is heavier
+	 * @return 2, if the names are equal in weight
+	 */
+	public int compareWeight(String name1, String name2) {
+		if (weigh(name1) > weigh(name2)) {
+			return 0;
+		}
+		else if (weigh(name1) < weigh(name2)) {
+			return 1;
+		}
+		else return 2;
+	}
 	
 	/**
 	 * @param String[] names, each element of which contains a single name. 
@@ -19,6 +74,25 @@ public class TheBestName {
 	 * @return the sorted String[].
 	 */
 	public String[] sort(String[] names) {
+		ArrayList<String> sorted = new ArrayList<String>();
+		for (int i = 0; i < names.length; i++) {
+			String name = names[i];
+			if (i == 0) {
+				sorted.add(name);
+			}
+			for (int j = 0; j < sorted.size(); j++) {
+				String sortedName = sorted.get(j);
+				// If name is "JOHN", put at the front of the list
+				if (name.equals("JOHN")) {
+					sorted.add(0, name);
+				}
+				// if sortedName is heavier than name
+				if (compareWeight(sortedName, name) == 0) {
+					sorted.add(j, name);
+				}
+				
+			}
+		}
 		// TODO Auto-generated constructor stub
 	}
 
