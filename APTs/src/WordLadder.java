938a64d6ceff9c941769db67720739638695a0b3
Keng <me@keng.me>
2013-12-31 08:59:50 -0500

    Initialized WordLadder

---
+++ APTs/src/WordLadder.java
@@ -0,0 +1,13 @@
+
+public class WordLadder {
+
+	public WordLadder() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
